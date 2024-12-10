f25c30d4f592f22f256f910978b6b3f87addd2bf
红色枫叶 <BlackMaple@live.cn>
2024-12-10 20:21:49 +0800

    2

---
+++ Maple.Nexomon.Windows/GameService/NexomonGameExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Maple.MonoGameAssistant.GameContext;
+using Maple.MonoGameAssistant.WebApi;
+using Maple.Nexomon.Windows.GameService;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Maple.Nexomon.GameService
+{
+    internal static partial class NexomonGameExtensions
+    {
+        [ModuleInitializer]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2255:不应在库中使用 “ModuleInitializer” 属性", Justification = "<挂起>")]
+        public static void Initializer()
+        {
+            _ = RunWebApiServiceAsync();
+
+            static async Task RunWebApiServiceAsync(int millisecondsDelay = 25 * 1000)
+            {
+
+                var webapp = WebApiServiceExtensions.AsRunWebApiService(static p =>
+                {
+                    p.GameName = "Nexomon: Extinction(尼可梦:毁灭)";
+                    p.MonoDataCollector = true;
+                    p.QQ = "^_^QQ:75351663";
+                    p.Http = true;
+                    p.AutoOpenUrl = true;
+                    p.Port = 49009;
+                }, static services => services.UseGameContextService<NexomonGameService>());
+                //延迟启动
+                await Task.Delay(millisecondsDelay).ConfigureAwait(false);
+                await webapp.RunAsync().ConfigureAwait(false);
+
+            }
+
+        }
+
+        [UnmanagedCallersOnly(CallConvs = [typeof(CallConvStdcall), typeof(CallConvSuppressGCTransition)], EntryPoint = nameof(DllMain))]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static bool DllMain(nint hModule, uint ul_reason_for_call, nint lpReserved)
+        {
+            return InitDllMain(hModule, ul_reason_for_call, lpReserved);
+        }
+
+
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvStdcall), typeof(CallConvSuppressGCTransition)])]
+        [LibraryImport("*")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool InitDllMain(nint hModule, uint ul_reason_for_call, nint lpReserved);
+
+    }
+
+
+
+
+}
---
+++ Maple.Nexomon.Windows/GameService/NexomonGameService.cs
@@ -0,0 +1,25 @@
+﻿using Maple.MonoGameAssistant.Core;
+using Maple.MonoGameAssistant.GameContext;
+using Maple.MonoGameAssistant.HotKey;
+using Maple.MonoGameAssistant.Model;
+using Maple.Nexomon.Metadata;
+using Microsoft.Extensions.Logging;
+
+namespace Maple.Nexomon.Windows.GameService
+{
+
+    internal sealed partial class NexomonGameService(ILogger<NexomonGameService> logger, MonoRuntimeContext runtimeContext, MonoTaskScheduler monoTaskScheduler, MonoGameSettings gameSettings, HookWinMsgFactory hookWinMsgFactory)
+        : GameContextService<NexomonMetadataContext>(logger, runtimeContext, monoTaskScheduler, gameSettings, hookWinMsgFactory)
+
+    {
+        protected sealed override NexomonMetadataContext LoadGameContext()
+        {
+            //    return NexomonMetadataContext.LoadGameContext(RuntimeContext, MonoGameAssistant.MonoCollectorDataV2.EnumMonoCollectorTypeVersion.APP, Logger);
+
+            return new NexomonMetadataContext(RuntimeContext, MonoGameAssistant.MonoCollectorDataV2.EnumMonoCollectorTypeVersion.APP, Logger);
+        }
+
+
+
+    }
+}
